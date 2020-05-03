9c3ef285c333ce4f9bc7695b38636921866682fe
Martin Page <59505434+martinjpage@users.noreply.github.com>
2020-05-03 14:47:21 +0200

    Firts commit of plot 2 code

---
+++ plot2.R
@@ -0,0 +1,31 @@
+##This script loads data and generates "Plot 2", a line graph of global active power over time from Feb 1 to Feb 2 2007
+
+#load libraries
+library(data.table)
+library(lubridate)
+library(dplyr)
+
+#read data
+powerdf <- fread(file = "household_power_consumption.txt", header = TRUE, na.strings = "?")
+
+#add new column for combined date-time
+powerdf$datetime <- dmy_hms(with(powerdf, paste(Date, Time)))
+
+#format original date and time variables
+powerdf$Date <- dmy(powerdf$Date) 
+powerdf$Time <- hms(powerdf$Time) 
+
+#subset data to include only 2 days - 1 Feb and 2 Feb 2007
+date_index <- c(dmy("01-02-2007"), dmy("02-02-2007"))
+subsetdf <- powerdf %>% filter(Date %in% date_index)
+
+#PLOT 2
+par(pty = "s", mfrow = c(1,1), mar = c(5.1, 4.1, 4.1, 2.1)) #set size to square (for later printing) and panel to 1 by 1 
+                                                            # and keep default margins
+
+plot(x= subsetdf$datetime, y = subsetdf$Global_active_power, type= "l", 
+     xlab = NA, ylab = "Global Active Power (kilowatts)")
+
+#save plot as PNG
+dev.print(png, file = "plot2.png", width = 480, height = 480)
+dev.off()
